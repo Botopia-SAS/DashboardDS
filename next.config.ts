6ca3ab9833e98c1c0447c0364ce1dffacc9de331
David Espejo <davas.espejo@gmail.com>
2025-02-05 10:17:16 -0500

    dashboard

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["res.cloudinary.com"],
+    domains: ["res.cloudinary.com"], // Permite imágenes desde Cloudinary
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Aplica a todas las rutas
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' http://localhost:3000 https://driving-school-mocha.vercel.app",
+          },
+        ],
+      },
+    ];
   },
 };
 
