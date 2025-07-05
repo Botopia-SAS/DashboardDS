bd2a4ba47891c3eba7f6edaedf9a5edc22801986
ANDREYPLAZAST <asptstevenplazas@gmail.com>
2025-07-05 15:09:01 -0500

    solcuion_types

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -64,7 +64,7 @@ export async function POST(req: NextRequest) {
     let isPasswordValid = false;
     try {
       isPasswordValid = await bcrypt.compare(password, admin.password);
-    } catch (e) {
+    } catch {
       isPasswordValid = false;
     }
     if (!isPasswordValid) {
